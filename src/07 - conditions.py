d19f08b980a49672bba3bbdbab261146ee2174c4
patrickjwolf <patrickwolf@Lenovo-PC.localdomain>
2020-07-06 16:20:44 -0600

    submission-1

--- src/07 - conditions.py
+++ src/07 - conditions.py
@@ -2,48 +2,48 @@
 Conditions
 """
 
-# # 1. Comparing or evaluating expressions
-# x = 2
-# print(x == 2)  # prints out True
-# print(x == 3)  # prints out False
-# print(x < 3)  # prints out True
+# 1. Comparing or evaluating expressions
+x = 2
+print(x == 2)  # prints out True
+print(x == 3)  # prints out False
+print(x < 3)  # prints out True
 
-# # 2. "and" & "or"
-# name = "Sean"
-# age = 31
-# if name == "Matt" and age == 31:
-#     print("Your name is Matt, and you are also 31 years old.")
+# 2. "and" & "or"
+name = "Sean"
+age = 31
+if name == "Matt" and age == 31:
+    print("Your name is Matt, and you are also 31 years old.")
 
-# if name == "Matt" or name == "Sean":
-#     print("Your name is either Matt or Sean.")
+if name == "Matt" or name == "Sean":
+    print("Your name is either Matt or Sean.")
 
-# # 3. "in"
-# name = "Matt"
-# if name in ["Matt", "Sean"]:
-#     print("Your name is either Matt or Sean.")
+# 3. "in"
+name = "Matt"
+if name in ["Matt", "Sean"]:
+    print("Your name is either Matt or Sean.")
 
-# # 4. "if", "elif", "else"
-# statement = False
-# another_statement = True
-# if statement is True:
-#     # do something
-#     pass
-# elif another_statement is True:  # else if
-#     print("another statement is True")
-#     pass
-# else:
-#     # do another thing
-#     pass
+# 4. "if", "elif", "else"
+statement = False
+another_statement = True
+if statement is True:
+    # do something
+    pass
+elif another_statement is True:  # else if
+    print("another statement is True")
+    pass
+else:
+    # do another thing
+    pass
 
-# # 5. "is" matches instances and not values
-# x = [1, 2, 3]
-# y = [1, 2, 3]
-# print(x == y)  # Prints out True
-# print(x is y)  # Prints out False
+# 5. "is" matches instances and not values
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(x == y)  # Prints out True
+print(x is y)  # Prints out False
 
-# # 6. "not" inverts a boolean
-# print(not False)  # Prints out True
-# print((not False) == (False))  # Prints out False
+# 6. "not" inverts a boolean
+print(not False)  # Prints out True
+print((not False) == (False))  # Prints out False
 
 
 """
